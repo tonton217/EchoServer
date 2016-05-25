ac1319a63012988478c93af15ea4cc1c504bad28
tonton217 <anthony.wonjay.jung@gmail.com>
2016-05-25 23:05:17 +0900

    c++ thread library using function object

--- src/main.cc
+++ src/main.cc
@@ -6,20 +6,37 @@
 
 using namespace std;
 
-void counter(int id, int numIterations) {
-  for (int i = 0; i < numIterations; ++i) {
-    cout << "Counter " << id << " has value ";
-    cout << i << endl;
-  }
-}
+class Counter {
+  public:
+    Counter(int id, int numIterations)
+      : id_(id), numIterations_(numIterations)
+    {}
+
+    void operator()() const {
+      for (int i = 0; i < numIterations_; ++i) {
+        cout << "Counter " << id_ << " has value ";
+        cout << i << endl;
+      }
+    }
+
+  private:
+    int id_;
+    int numIterations_;
+};
 
 int main() {
   cout.sync_with_stdio(true);
-  thread t1(counter, 1, 6);
-  thread t2(counter, 2, 4);
+
+  thread t1{Counter(1, 20)};
+
+  Counter c(2, 12);
+  thread t2(c);
+
+  thread t3(Counter(3, 10));
 
   t1.join();
   t2.join();
+  t3.join();
 
   return 0;
 }
